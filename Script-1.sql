ffa456747dd2ff0e4b331c6b043c1eabe421b08c
Sofisnake <Sofi.m.d.v@yandex.ru>
2023-07-22 13:42:54 +0300

    Script

---
+++ Script-1.sql
@@ -0,0 +1,214 @@
+CREATE TABLE IF NOT EXISTS Genre (
+id serial PRIMARY KEY,
+name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Executor (
+id serial PRIMARY KEY,
+name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ExecutorGenre (
+genre_id INTEGER REFERENCES Genre(id),
+executor_id INTEGER REFERENCES Executor(id),
+constraint pk primary key (genre_id, executor_id)
+);
+
+CREATE TABLE IF NOT EXISTS Album (
+id serial PRIMARY KEY,
+name VARCHAR(60) NOT NULL,
+create_date date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS AlbumExecutor (
+album_id INTEGER REFERENCES Album(id),
+executor_id INTEGER REFERENCES Executor(id),
+constraint pk_AlbumExecutor primary key (album_id, executor_id)
+);
+
+CREATE TABLE IF NOT EXISTS Song (
+id serial PRIMARY KEY,
+name_song VARCHAR(60) NOT NULL,
+time_song TIME NOT NULL,
+album_id INTEGER NOT NULL REFERENCES Album(id)
+);
+
+CREATE TABLE IF NOT EXISTS SongsBook (
+id serial PRIMARY KEY,
+name_songsBook VARCHAR(60) NOT NULL,
+date_create date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS SongInSongsBook (
+song_id INTEGER REFERENCES Song(id),
+songsbook_id INTEGER REFERENCES SongsBook(id),
+constraint pk_SongInSongsBook primary key (song_id, songsbook_id)
+);
+
+INSERT INTO executor(name)
+VALUES('Zivert');
+
+INSERT INTO executor(name)
+VALUES('Руки вверх');
+
+INSERT INTO executor(name)
+VALUES('Сплин');
+
+INSERT INTO executor(name)
+VALUES('Кино');
+
+INSERT INTO executor(name)
+VALUES('Монеточка');
+
+INSERT INTO genre(name)
+VALUES('Рок');
+
+INSERT INTO genre(name)
+VALUES('Инди');
+
+INSERT INTO genre(name)
+VALUES('ПОП');
+
+INSERT INTO album(name, create_date)
+VALUES('Расскраски для взрослых', '25.05.2018');
+
+INSERT INTO album
+VALUES(9080, 'Расскраски', '25.05.2019');
+
+INSERT INTO album
+VALUES(120, 'Сделай погромче!', '25.05.1998');
+
+INSERT INTO album
+VALUES(2,'Сияй', '06.04.2018');
+
+INSERT INTO album
+VALUES(7, 'Легенда','19.05.2018');
+
+INSERT INTO album
+VALUES(4, 'Гранатовый альбом', '01.04.1998');
+
+INSERT INTO song
+VALUES(1179, 'ляляля', '00:02:59.0000', 9080);
+
+INSERT INTO song(name_song, time_song, album_id)
+VALUES('Нимфоманка', '00:02:40.0000', 1);
+
+INSERT INTO song
+VALUES(11, 'Каждый раз', '00:03:28.0000', 1);
+
+INSERT INTO song(name_song, time_song, album_id)
+VALUES('Сияй','00:03:24.0000', 2);
+
+INSERT INTO song(name_song, time_song, album_id)
+VALUES('Океан','00:03:24.0000', 2);
+
+INSERT INTO song
+VALUES(30, 'Кукушка','00:06:40.0000', 7);
+
+INSERT INTO song
+VALUES(987, 'Группа крови','00:03:59.0000', 7);
+
+INSERT INTO song(name_song, time_song, album_id)
+VALUES('Приходи','00:04:02.0000', 4);
+
+INSERT INTO song
+VALUES(100, 'Выхода нет','00:03:47.0000', 4);
+
+INSERT INTO song
+VALUES(19, 'Мой родной','00:03:57.0000', 4);
+
+INSERT INTO song
+VALUES(1121, 'Крошка моя', '00:04:10.0000', 120);
+
+INSERT INTO song
+VALUES(1122, 'Чужие губы', '00:04:02.0000', 120);
+
+INSERT INTO songsbook(name_songsBook, date_create)
+VALUES('На закате', '19.05.2020');
+
+INSERT INTO songsbook(name_songsBook, date_create)
+VALUES('Рассвет', '01.06.2019');
+
+INSERT INTO songsbook(name_songsBook, date_create)
+VALUES('Грусть', '10.09.2020');
+
+INSERT INTO songsbook(name_songsBook, date_create)
+VALUES('Воспоминания из детства', '19.05.2021');
+
+INSERT INTO albumexecutor (album_id, executor_id)
+VALUES(1, 4);
+
+INSERT INTO albumexecutor (album_id, executor_id)
+VALUES(9080, 4);
+
+INSERT INTO albumexecutor (album_id, executor_id)
+VALUES(2, 1);
+
+INSERT INTO albumexecutor (album_id, executor_id)
+VALUES(7, 3);
+
+INSERT INTO albumexecutor (album_id, executor_id)
+VALUES(4, 2);
+
+INSERT INTO albumexecutor (album_id, executor_id)
+VALUES(120, 5);
+
+INSERT INTO executorgenre  (executor_id, genre_id )
+VALUES(1, 3);
+
+INSERT INTO executorgenre (executor_id, genre_id )
+VALUES(2, 1);
+
+INSERT INTO executorgenre (executor_id, genre_id )
+VALUES(3, 1);
+
+INSERT INTO executorgenre (executor_id, genre_id )
+VALUES(4, 2);
+
+INSERT INTO executorgenre (executor_id, genre_id )
+VALUES(5, 3);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(1179, 3);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(2, 1);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(11, 1);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(3, 3);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(4, 3);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(30, 2);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(987, 2);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(5, 2);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(100, 2);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(19, 4);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(100, 4);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(5, 4);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(30, 4);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(1121, 2);
+
+INSERT INTO songinsongsbook (song_id, songsbook_id)
+VALUES(1122, 2);
---
+++ Script-2.sql
@@ -0,0 +1,76 @@
+-- Название и продолжительность самого длительного трека.
+SELECT name_song, time_song FROM song
+ORDER BY time_song DESC
+LIMIT 1;
+
+-- Название треков, продолжительность которых не менее 3,5 минут.
+SELECT name_song FROM song
+WHERE time_song >= '00:03:30';
+
+
+-- Названия сборников, вышедших в период с 2018 по 2020 год включительно.
+SELECT name_songsbook FROM songsbook
+WHERE date_create <= '31.12.2020' AND date_create >= '01.01.2018' ;
+
+SELECT name_songsbook FROM songsbook
+WHERE date_create BETWEEN '01.01.2018' AND '31.12.2020' ;
+
+-- Исполнители, чьё имя состоит из одного слова.
+SELECT name FROM executor
+WHERE NOT name LIKE '%% %%';
+
+-- Название треков, которые содержат слово «мой» или «my».
+SELECT name_song FROM song
+WHERE name_song ILIKE 'Мой_%' 
+OR name_song ILIKE '%_мой'
+OR name_song ILIKE '%_мой_%'
+OR name_song ILIKE 'мой'
+OR name_song ILIKE 'my_%'
+OR name_song ILIKE '%_my'
+OR name_song ILIKE '%_my_%'
+OR name_song ILIKE 'my';
+
+
+-- Количество исполнителей в каждом жанре.
+SELECT g.name, count(e.name) FROM genre AS g
+LEFT JOIN executorgenre AS eg ON g.id = eg.genre_id 
+LEFT JOIN executor AS e ON eg.executor_id = e.id 
+GROUP BY g.name 
+ORDER BY count(e.id) DESC ;
+
+-- Количество треков, вошедших в альбомы 2019–2020 годов.
+SELECT s.name_song, a.create_date FROM album a 
+LEFT JOIN song s ON s.album_id = a.id 
+WHERE a.create_date >= '01.01.2019' AND a.create_date <='31.12.2020';
+
+
+
+-- Средняя продолжительность треков по каждому альбому.
+SELECT a.name, AVG(s.time_song) FROM album a 
+LEFT JOIN song s ON s.album_id = a.id 
+GROUP BY a.name
+ORDER BY avg(s.time_song);
+
+
+
+-- Все исполнители, которые не выпустили альбомы в 2020 году.
+SELECT DISTINCT e.name FROM executor e 
+WHERE e.name NOT IN (
+	SELECT DISTINCT e.name FROM executor e
+	LEFT JOIN albumexecutor ae ON e.id = ae.executor_id
+	LEFT JOIN album a ON a.id = ae.album_id
+	WHERE a.create_date >= '01.01.2020' OR a.create_date <='31.12.2020'
+	);
+
+
+
+-- Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
+SELECT DISTINCT sb.name_songsbook FROM songsbook sb
+LEFT JOIN songinsongsbook sisb ON sb.id = sisb.songsbook_id 
+LEFT JOIN song s ON s.id = sisb.song_id 
+LEFT JOIN album a ON a.id = s.album_id 
+LEFT JOIN albumexecutor ae ON ae.album_id = a.id 
+LEFT JOIN executor e ON e.id = ae.executor_id 
+WHERE e.name LIKE '%%Сплин%%'
+ORDER BY sb.name_songsbook ;
+	
